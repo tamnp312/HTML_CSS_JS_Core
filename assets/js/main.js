7468eda29f2eb3b64d912779bac4ecf79a169bda
tamnp312 <tamnp312@gmail.com>
2025-04-09 13:12:42 +0700

    create services

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,14 @@ fetch("../../components/header.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("header").innerHTML = data;
+
+    const navLinks = document.querySelectorAll(".nav__link");
+    navLinks.forEach((link, index) => {
+      link.addEventListener("click", () => {
+        navLinks.forEach((link) => link.classList.remove("nav__link--active"));
+        link.classList.add("nav__link--active");
+      });
+    });
   })
   .catch((error) => console.error("Error loading header:", error));
 
@@ -18,24 +26,42 @@ const tabContentsData = [
   "While you're on holiday, here's where your furry friends will spend their time 3.",
 ];
 
+const checkListData = [
+  "Certified Groomer",
+  "Animal Lover",
+  "14+ Years Experience",
+  "Pet Parent Of 3 Dogs",
+];
+
 fetch("../../components/about.html")
   .then((response) => response.text())
   .then((data) => {
     document.getElementById("about").innerHTML = data;
 
-    const tabContentsWrapper = document.querySelector('.tab__contents');
-    tabContentsWrapper.innerHTML = tabContentsData.map((text, index) => `
-    <div class="tab__content-item ${index === 0 ? 'tab__content-item--active' : ''}">
+    const aboutCheckList = document.querySelector(".about__checklist");
+    aboutCheckList.innerHTML = checkListData
+      .map(
+        (text) => `
+      <span class="about__check-item"> ${text} </span>`
+      )
+      .join("");
+
+    const tabContentsWrapper = document.querySelector(".tab__contents");
+    tabContentsWrapper.innerHTML = tabContentsData
+      .map(
+        (text, index) => `
+    <div class="tab__content-item ${
+      index === 0 ? "tab__content-item--active" : ""
+    }">
       <p class="tab__content-desc">${text}</p>
       <a href="#!" class="tab__content-more">View more</a>
     </div>
-  `).join('');
+  `
+      )
+      .join("");
 
     const tabs = document.querySelectorAll(".tab__item");
     const tabContent = document.querySelectorAll(".tab__content-item");
-
-    console.log("tabs", tabs);
-    console.log("tabContent", tabContent);
 
     tabs.forEach((tab, index) => {
       tab.addEventListener("click", () => {
@@ -50,3 +76,76 @@ fetch("../../components/about.html")
     });
   })
   .catch((error) => console.error("Error loading about:", error));
+
+// Services
+
+const serviceItemsData = [
+  {
+    title: "Puppy Sitting",
+    description:
+      "Helping your new pup to be a good boy or girl isn't always easy, but we're here to help.",
+    link: "#!",
+  },
+  {
+    title: "Dog Walking",
+    description:
+      "Choose from a 30, 45, or 60-minute visit to give your pet their daily dose of fun-filled exercise.",
+    link: "#!",
+  },
+  {
+    title: "Pet Sitting",
+    description:
+      "While you're away we can make sure your pet has all the food, water, exercise, and care they need.",
+    link: "#!",
+  },
+  {
+    title: "Overnight Care",
+    description:
+      "If you're away for the night, we can stay the night or stop by in the evening and morning.",
+    link: "#!",
+  },
+  {
+    title: "Pet Taxi",
+    description:
+      "Does your pet need a lift to the groomers, vet, or dog park? We've got their tail covered.",
+
+    link: "#!",
+  },
+  {
+    title: "Pet Medical Admin",
+    description:
+      "Our team of experienced professionals can help with everything from pills to injections.",
+    link: "#!",
+  },
+];
+
+fetch("../../components/services.html")
+  .then((response) => response.text())
+  .then((data) => {
+    document.getElementById("services").innerHTML = data;
+
+    const serviceItemsWrapper1 = document.querySelector(".service__column1");
+    serviceItemsWrapper1.innerHTML = serviceItemsData.slice(0, 3).map((item) => {
+      return ` 
+       <article class="service__item"> 
+        <h3 class="service__item--heading">${item.title}</h3>
+        <p class="service__item--desc">${item.description}</p>
+        <a href="${item.link}" class="service__item--more">Read more</a>
+       </article>
+      `;
+    }).join('');
+
+    const serviceItemsWrapper2 = document.querySelector(".service__column2");
+    serviceItemsWrapper2.innerHTML = serviceItemsData.slice(3).map((item) => {
+      return ` 
+       <article class="service__item"> 
+        <h3 class="service__item--heading">${item.title}</h3>
+        <p class="service__item--desc">${item.description}</p>
+        <a href="${item.link}" class="service__item--more">Read more</a>
+       </article>
+      `;
+    }).join('');
+  })
+  .catch((error) => {
+    console.error("Error loading services:", error);
+  });
